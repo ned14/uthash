ff1f0adecebcac0c8c5b75e448a955618be1c7d8
Troy D. Hanson <tdh at tkhanson.net>
2008-07-18 05:12:43 +0000

    Test for HASH_DEL(users,users) problem scenario fixed in pending commit.
    Detect MinGW in tests and divert (no perl there).
    Detect MinGW in keystat.c and use alt gettimeofday().

--- tests/keystat.c
+++ tests/keystat.c
@@ -8,6 +8,26 @@
 #include <sys/time.h>   /* for 'gettimeofday' */
 #include "uthash.h"
 
+/* Windows doesn't have gettimeofday() */
+#if ( defined __MINGW32__ )
+#include <windows.h>
+#include <sys/time.h>
+
+void __stdcall GetSystemTimeAsFileTime(FILETIME*);
+
+void gettimeofday(struct timeval* p, void* tz /* IGNORED */)
+{
+  union {
+    long long ns100; /*time since 1 Jan 1601 in 100ns units */
+    FILETIME ft;
+  } now;
+
+  GetSystemTimeAsFileTime( &(now.ft) );
+  p->tv_usec=(long)((now.ns100 / 10LL) % 1000000LL );
+  p->tv_sec= (long)((now.ns100-(116444736000000000LL))/10000000LL);
+}
+#endif
+
 #ifndef timersub
 #define timersub(a, b, result)                                                \
   do {                                                                        \
