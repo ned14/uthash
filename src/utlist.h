1099bf8e79c57502cad9b9a6b56352194846d6af
Troy D. Hanson <tdh at tkhanson.net>
2009-05-21 01:24:56 +0000

    strict-aliasing compliant utlist.h

--- src/utlist.h
+++ src/utlist.h
@@ -24,14 +24,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef UTLIST_H
 #define UTLIST_H
 
-#define UTLIST_VERSION 1.0
+#define UTLIST_VERSION 1.7
 
-/* C++ requires extra stringent casting */
-#if defined __cplusplus
-#define LTYPEOF(x) (typeof(x))
-#else
-#define LTYPEOF(x)
-#endif
 /* 
  * This file contains macros to manipulate singly and doubly-linked lists.
  *
@@ -64,30 +58,16 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 /******************************************************************************
- * The SORT macros                                                            *
- *****************************************************************************/
-#define LL_SORT(l,cmp)                                                           \
- LISTSORT(l,0,0,FIELD_OFFSET(l,next),cmp)
-#define DL_SORT(l,cmp)                                                           \
- LISTSORT(l,0,FIELD_OFFSET(l,prev),FIELD_OFFSET(l,next),cmp)
-#define CDL_SORT(l,cmp)                                                          \
- LISTSORT(l,1,FIELD_OFFSET(l,prev),FIELD_OFFSET(l,next),cmp)
-
-/* The macros can't assume or cast to the caller's list element type. So we use
- * a couple tricks when we need to deal with those element's prev/next pointers.
- * Basically we use char pointer arithmetic to get those field offsets. */
-#define FIELD_OFFSET(ptr,field) ((char*)&((ptr)->field) - (char*)(ptr))
-#define LNEXT(e,no) (*(char**)(((char*)e) + no))
-#define LPREV(e,po) (*(char**)(((char*)e) + po))
-/******************************************************************************
  * The LISTSORT macro is an adaptation of Simon Tatham's O(n log(n)) mergesort*
  * Unwieldy variable names used here to avoid shadowing passed-in variables.  *
  *****************************************************************************/
-#define LISTSORT(list, is_circular, po, no, cmp)                                 \
+#define LL_SORT(l,cmp)  LISTSORT(l,0,0,cmp)
+#define DL_SORT(l,cmp)  LISTSORT(l,0,1,cmp)
+#define CDL_SORT(l,cmp) LISTSORT(l,1,1,cmp)
+#define LISTSORT(list, is_circular, is_double, cmp)                             \
 do {                                                                             \
-  void *_ls_p, *_ls_q, *_ls_e, *_ls_tail, *_ls_oldhead;                          \
+  __typeof__(list) _ls_p, _ls_q, _ls_e, _ls_tail, _ls_oldhead;              \
   int _ls_insize, _ls_nmerges, _ls_psize, _ls_qsize, _ls_i, _ls_looping;         \
-  int _ls_is_double = (po==0) ? 0 : 1;                                           \
   if (list) {                                                                    \
     _ls_insize = 1;                                                              \
     _ls_looping = 1;                                                             \
@@ -104,46 +84,46 @@ do {                                                                            
         for (_ls_i = 0; _ls_i < _ls_insize; _ls_i++) {                           \
           _ls_psize++;                                                           \
           if (is_circular)  {                                                    \
-            _ls_q = ((LNEXT(_ls_q,no) == _ls_oldhead) ? NULL : LNEXT(_ls_q,no)); \
+            _ls_q = ((_ls_q->next == _ls_oldhead) ? NULL : _ls_q->next); \
           } else  {                                                              \
-            _ls_q = LNEXT(_ls_q,no);                                             \
+            _ls_q = _ls_q->next;                                             \
           }                                                                      \
           if (!_ls_q) break;                                                     \
         }                                                                        \
         _ls_qsize = _ls_insize;                                                  \
         while (_ls_psize > 0 || (_ls_qsize > 0 && _ls_q)) {                      \
           if (_ls_psize == 0) {                                                  \
-            _ls_e = _ls_q; _ls_q = LNEXT(_ls_q,no); _ls_qsize--;                 \
+            _ls_e = _ls_q; _ls_q = _ls_q->next; _ls_qsize--;                 \
             if (is_circular && _ls_q == _ls_oldhead) { _ls_q = NULL; }           \
           } else if (_ls_qsize == 0 || !_ls_q) {                                 \
-            _ls_e = _ls_p; _ls_p = LNEXT(_ls_p,no); _ls_psize--;                 \
+            _ls_e = _ls_p; _ls_p = _ls_p->next; _ls_psize--;                 \
             if (is_circular && (_ls_p == _ls_oldhead)) { _ls_p = NULL; }         \
-          } else if (cmp(LTYPEOF(list)_ls_p,LTYPEOF(list)_ls_q) <= 0) {          \
-            _ls_e = _ls_p; _ls_p = LNEXT(_ls_p,no); _ls_psize--;                 \
+          } else if (cmp(_ls_p,_ls_q) <= 0) {          \
+            _ls_e = _ls_p; _ls_p = _ls_p->next; _ls_psize--;                 \
             if (is_circular && (_ls_p == _ls_oldhead)) { _ls_p = NULL; }         \
           } else {                                                               \
-            _ls_e = _ls_q; _ls_q = LNEXT(_ls_q,no); _ls_qsize--;                 \
+            _ls_e = _ls_q; _ls_q = _ls_q->next; _ls_qsize--;                 \
             if (is_circular && (_ls_q == _ls_oldhead)) { _ls_q = NULL; }         \
           }                                                                      \
           if (_ls_tail) {                                                        \
-            LNEXT(_ls_tail,no) = (char*)_ls_e;                                   \
+            _ls_tail->next = _ls_e;                                   \
           } else {                                                               \
-            list = LTYPEOF(list)_ls_e;                                           \
+            list = _ls_e;                                           \
           }                                                                      \
-          if (_ls_is_double) {                                                   \
-            LPREV(_ls_e,po) = (char*)_ls_tail;                                   \
+          if (is_double) {                                                   \
+            _ls_e->prev = _ls_tail;                                   \
           }                                                                      \
           _ls_tail = _ls_e;                                                      \
         }                                                                        \
         _ls_p = _ls_q;                                                           \
       }                                                                          \
       if (is_circular) {                                                         \
-        LNEXT(_ls_tail,no) = (char*)list;                                        \
-        if (_ls_is_double) {                                                     \
-          LPREV(list,po) = (char*)_ls_tail;                                      \
+        _ls_tail->next = list;                                        \
+        if (is_double) {                                                     \
+          list->prev = _ls_tail;                                      \
         }                                                                        \
       } else  {                                                                  \
-        LNEXT(_ls_tail,no) = NULL;                                               \
+        _ls_tail->next = NULL;                                               \
       }                                                                          \
       if (_ls_nmerges <= 1) {                                                    \
         _ls_looping=0;                                                           \
@@ -164,12 +144,12 @@ do {                                                                            
 
 #define LL_APPEND(head,add)                                                      \
 do {                                                                             \
+  __typeof__(head) _tmp;                                                         \
   (add)->next=NULL;                                                              \
   if (head) {                                                                    \
-    char *_lla_el = (char*)(head);                                               \
-    unsigned _lla_no = FIELD_OFFSET(head,next);                                  \
-    while (LNEXT(_lla_el,_lla_no)) { _lla_el = LNEXT(_lla_el,_lla_no); }         \
-    LNEXT(_lla_el,_lla_no)=(char*)(add);                                         \
+    _tmp = head;                                                                 \
+    while (_tmp->next) { _tmp = _tmp->next; }         \
+    _tmp->next=(add);                                         \
   } else {                                                                       \
     (head)=(add);                                                                \
   }                                                                              \
@@ -177,16 +157,16 @@ do {                                                                            
 
 #define LL_DELETE(head,del)                                                      \
 do {                                                                             \
+  __typeof__(head) _tmp;                                                         \
   if ((head) == (del)) {                                                         \
     (head)=(head)->next;                                                         \
   } else {                                                                       \
-    char *_lld_el = (char*)(head);                                               \
-    unsigned _lld_no = FIELD_OFFSET(head,next);                                  \
-    while (LNEXT(_lld_el,_lld_no) && (LNEXT(_lld_el,_lld_no) != (char*)(del))) { \
-      _lld_el = LNEXT(_lld_el,_lld_no);                                          \
+    _tmp = head;                                                                 \
+    while (_tmp->next && (_tmp->next != (del))) { \
+      _tmp = _tmp->next;                                         \
     }                                                                            \
-    if (LNEXT(_lld_el,_lld_no)) {                                                \
-      LNEXT(_lld_el,_lld_no) = (char*)((del)->next);                             \
+    if (_tmp->next) {                                                \
+      _tmp->next = ((del)->next);                             \
     }                                                                            \
   }                                                                              \
 } while (0)
